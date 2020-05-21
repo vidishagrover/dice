5ffa26e9c315c551022b72f24d27dcc77cff8e44
vidishagrover <62940133+vidishagrover@users.noreply.github.com>
2020-05-21 17:01:58 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDice = "dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img") [0];
+image1.setAttribute("src", randomDice);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDice1 = "dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img") [1];
+image2.setAttribute("src", randomDice1);
+
+if( randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩PLAYER 1 WINS!!"
+}
+else if( randomNumber2 > randomNumber1   ){
+    document.querySelector("h1").innerHTML = "🚩PLAYER 2 WINS!!"
+}
+else{
+    document.querySelector("h1").innerHTML = "🤪DRAW!!"
+}
